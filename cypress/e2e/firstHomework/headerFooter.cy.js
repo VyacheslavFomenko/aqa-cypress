55afa9fb76f7bca91836d86421e49bcc75055a35
Vyacheslav Fomenko <playfog3@gmail.com>
2025-04-21 19:07:57 +0300

    add: find header buttons test

---
+++ cypress/e2e/firstHomework/headerFooter.cy.js
@@ -0,0 +1,33 @@
+describe("Find Header and Footer elements", () => {
+    beforeEach(() => {
+        cy.visit("https://qauto.forstudy.space/", {
+            auth: {
+                username: "guest",
+                password: "welcome2qauto"
+            }
+        });
+    })
+
+    it("should find all buttons in the header", () => {
+        cy.get("header").should("be.visible");
+
+        cy.get("button")
+            .should("have.length.at.least", 1)
+            .each(($btn) => {
+                cy.wrap($btn)
+                    .should("be.visible")
+                    .and("not.be.disabled");
+            });
+
+        cy.get("a")
+            .should("have.length.at.least", 1)
+            .each(($link) => {
+                cy.wrap($link)
+                    .should("be.visible")
+                    .and("have.attr", "href")
+                    .and("not.be.empty");
+            });
+    });
+});
+
+
