c5a6c23ab1693b61ba5932516f3c179dd54e5c0b
Vyacheslav Fomenko <playfog3@gmail.com>
2025-04-26 10:34:17 +0300

    add: check of name field when less than minimum of requirement amount of symbols

--- cypress/e2e/second-homework/registration.cy.js
+++ cypress/e2e/second-homework/registration.cy.js
@@ -1,4 +1,4 @@
-import { faker } from "@faker-js/faker";
+import {faker} from "@faker-js/faker";
 
 describe("Check Registration flow", () => {
     beforeEach(() => {
@@ -15,12 +15,18 @@ describe("Check Registration flow", () => {
         cy.contains("Registration").should("be.visible");
     });
 
-    context("Name field validation", ()=>{
+    context("Name field validation", () => {
         const nameInput = () => cy.get("#signupName");
 
-        it("show error when empty", ()=>{
+        it("should show error when empty", () => {
             nameInput().focus().blur();
             cy.contains("Name required").should("be.visible");
+            nameInput().should("have.css", "border-color", "rgb(220, 53, 69)");
+        });
+
+        it("should show error when less than 2 characters long", () => {
+            nameInput().type("A").blur();
+            cy.contains("Name has to be from 2 to 20 characters long").should("be.visible");
             nameInput().should("have.css", "border-color", "rgb(220, 53, 69)");
         });
     });
