6070af68ad94b397c7dcb03ab626e1c5122b013c
Vyacheslav Fomenko <playfog3@gmail.com>
2025-05-04 20:15:31 +0300

    add: reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,14 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
     e2e: {
         // Configure your E2E tests here
-        specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
+        specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+        reporter: "mochawesome",
+        reporterOptions: {
+            reportDir: "mochawesome-report",
+            overwrite: false,
+            html: true,
+            json: true
+        }
     },
+
 });
