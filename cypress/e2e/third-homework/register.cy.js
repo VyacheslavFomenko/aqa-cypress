0b5b6b56d1249bab6162622d0886e3107ad33316
Vyacheslav Fomenko <playfog3@gmail.com>
2025-05-05 19:11:59 +0300

    add: two config generators

--- cypress/e2e/third-homework/register.cy.js
+++ cypress/e2e/third-homework/register.cy.js
@@ -5,5 +5,4 @@ describe("Register users for config file", ()=>{
    it("register to second site", ()=>{
       cy.register({ url: "https://qauto2.forstudy.space/", name: "Tom", lastName: "Ball", email: "qwerty@gmail.com", password: "nBZCa8!ST5WfBHC"});
    });
-
 });
