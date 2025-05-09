f520a894dba653228a2fe27f710e3690640dced7
Vyacheslav Fomenko <playfog3@gmail.com>
2025-05-09 19:45:48 +0300

    add: login

--- cypress/e2e/third-homework/garageAndExpenses.cy.js
+++ cypress/e2e/third-homework/garageAndExpenses.cy.js
@@ -1,14 +1,20 @@
 import GaragePage from './garagePage';
 import ExpensesPage from './fuelExpensesPage';
+
 const config = require("/config/qauto.config.js");
 
-describe("", ()=>{
+describe("", () => {
     beforeEach(() => {
-        cy.visit(config.baseUrl);
+        cy.visit(config.baseUrl, {
+            auth: {
+                username: "guest",
+                password: "welcome2qauto"
+            }
+        });
         cy.login({email: config.user.email, password: config.user.password});
     });
 
-    it("should successfully add car to garage and fuel Expenses", ()=>{
+    it("should successfully add car to garage and fuel Expenses", () => {
         GaragePage.addCar(50);
         ExpensesPage.addFuelExpenses({vehicle: "Audi TT", date: "25.04.2000", mileage: 50, cost: 1000});
     });
